e5fc2cdb7e623cdfd9e6b97e4252ddd638b73311
MEAKSOY <mesataksoy@gmail.com>
2022-05-20 17:47:53 +0000

    Files are updated.

--- review_script.sh
+++ review_script.sh
@@ -1,6 +1,16 @@
 #! /bin/bash -x
 
+echo "Muhammet Esat AKSOY"
+
+cat << EOF
+Wellcome to the Linux Lesson 6.
+This lesson is about the shell scripting 
+EOF
+
+<< comment
 echo "World"
 date
 pwd
 ls
+comment
+
