5c696753eea3dcb7781e14f4f2e7738c2bdf6cf5
MEAKSOY <mesataksoy@gmail.com>
2022-07-11 20:50:07 +0200

    file is modified.

--- sh_script_practice/ping_script.sh
+++ sh_script_practice/ping_script.sh
@@ -4,7 +4,7 @@
 # Description: This script will ping a remote host and notify
 # Modified: 11/7/2022
 
-hosts="192.168.2.7"
+hosts="2a00:1450:400e:801::200e:"
 
 ping $hosts &> /dev/null
 	if [ $? -eq 0 ]
@@ -13,3 +13,4 @@ ping $hosts &> /dev/null
 	else
 	echo "$hosts is not OK"
 	fi
+
